8f800e02a378c35cae63e37ac1151ff1971a65cb
mabubakarsiddiq11 <mabubakarsiddiq11@gmail.com>
2025-01-17 23:54:14 +0500

    Basic Routing

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-center text-7xl text-red-500 font-bold">Hello About Page!</h1>
+    </div>
+  )
+}
---
+++ src/app/profile/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div>
+      <h1 className="text-center text-7xl text-red-500 font-bold">Hello Profile Page!</h1>
+    </div>
+  )
+}
